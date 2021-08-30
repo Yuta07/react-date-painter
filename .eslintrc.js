79e92e685d66edc063d739189e23d1b31dfd9e45
yutaka.miyazaki <mono.12dev@gmail.com>
2021-08-30 21:59:29 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 7,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'prettier',
+	],
+	plugins: ['prettier', 'react'],
+	rules: {},
+	settings: {
+		react: {
+			version: 'detect', // detect the version of React to use
+		},
+	},
+}
