5908102d8f80b3a822e5c78746b034816f429d56
yutaka.miyazaki <mono.12dev@gmail.com>
2021-09-08 23:51:56 +0900

    feat: add jest

---
+++ react-date-painter/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { DatePainter } from '../src/index'
+
+describe('React Date Painter test', () => {
+	test('only render', () => {
+		render(<DatePainter selectedDates={[]} />)
+	})
+})
