f7c65857a7554efe6ec377c500d9a0542fa867ec
yutaka.miyazaki <mono.12dev@gmail.com>
2021-09-10 23:26:02 +0900

    feat: add body style test

--- react-date-painter/__tests__/App.test.tsx
+++ react-date-painter/__tests__/App.test.tsx
@@ -5,50 +5,98 @@ import '@testing-library/jest-dom'
 
 import { DatePainter } from '../src/index'
 import { extractDayOfWeekList } from '../src/utils/calendarHelper'
-
-const now = dayjs().year(2021).month(8)
+import { createCurrentMonthCalendar } from '../src/utils/createCurrentMonthCalendar'
 
 afterEach(cleanup)
 
 describe('ReactDatePainter', () => {
-	test('test calendar month format', () => {
-		const currentMonth = now.format('MMMM YYYY')
+	const now = dayjs()
 
-		const screen = render(<DatePainter selectedDates={[]} />)
-		const element = screen.getByTestId('current-month')
+	describe('calendar header test', () => {
+		test('test calendar month format', () => {
+			const currentMonth = now.format('MMMM YYYY')
 
-		expect(element).toHaveTextContent(currentMonth)
+			const screen = render(<DatePainter selectedDates={[]} />)
+			const element = screen.getByTestId('current-month')
 
-		fireEvent.click(screen.getByTestId('previous-button'))
-		const previousMonth = now.subtract(1, 'month').format('MMMM YYYY')
-		expect(screen.getByTestId('current-month')).toHaveTextContent(previousMonth)
+			expect(element).toHaveTextContent(currentMonth)
 
-		fireEvent.click(screen.getByTestId('next-button'))
-		expect(screen.getByTestId('current-month')).toHaveTextContent(currentMonth)
+			fireEvent.click(screen.getByTestId('previous-button'))
+			const previousMonth = now.subtract(1, 'month').format('MMMM YYYY')
+			expect(screen.getByTestId('current-month')).toHaveTextContent(previousMonth)
 
-		fireEvent.click(screen.getByTestId('next-button'))
-		const nextMonth = now.add(1, 'month').format('MMMM YYYY')
-		expect(screen.getByTestId('current-month')).toHaveTextContent(nextMonth)
+			fireEvent.click(screen.getByTestId('next-button'))
+			expect(screen.getByTestId('current-month')).toHaveTextContent(currentMonth)
+
+			fireEvent.click(screen.getByTestId('next-button'))
+			const nextMonth = now.add(1, 'month').format('MMMM YYYY')
+			expect(screen.getByTestId('current-month')).toHaveTextContent(nextMonth)
+		})
+
+		test('test custom calendar month format', () => {
+			const currentMonth = now.format('YYYY/MM')
+
+			const screen = render(<DatePainter selectedDates={[]} format="YYYY/MM" />)
+			const element = screen.getByTestId('current-month')
+
+			expect(element).toHaveTextContent(currentMonth)
+		})
 	})
 
-	test('test custom calendar month format', () => {
-		const currentMonth = now.format('YYYY/MM')
+	describe('calendar week of day test', () => {
+		test('test day of week display', () => {
+			const expectedDayOfWeek = ['日', '月', '火', '水', '木', '金', '土']
 
-		const screen = render(<DatePainter selectedDates={[]} format="YYYY/MM" />)
-		const element = screen.getByTestId('current-month')
+			const element = extractDayOfWeekList()
+			expect(element).toEqual(expect.arrayContaining(expectedDayOfWeek))
 
-		expect(element).toHaveTextContent(currentMonth)
+			const screen = render(<DatePainter selectedDates={[]} />)
+			expect(screen.getByTestId('day-week-0')).toHaveTextContent('日')
+			expect(screen.getByTestId('day-week-3')).toHaveTextContent('水')
+			expect(screen.getByTestId('day-week-6')).toHaveTextContent('土')
+		})
 	})
 
-	test('test day of week display', () => {
-		const expectedDayOfWeek = ['日', '月', '火', '水', '木', '金', '土']
+	describe('calendar body current date test', () => {
+		const day = now.format('M/D')
 
-		const element = extractDayOfWeekList()
-		expect(element).toEqual(expect.arrayContaining(expectedDayOfWeek))
+		test('test body today', () => {
+			const screen = render(<DatePainter selectedDates={[]} />)
+			const element = screen.getByTestId(`date-txt-${day}`)
+			expect(element).toHaveClass('today')
+		})
 
-		const screen = render(<DatePainter selectedDates={[]} />)
-		expect(screen.getByTestId('day-week-0')).toHaveTextContent('日')
-		expect(screen.getByTestId('day-week-3')).toHaveTextContent('水')
-		expect(screen.getByTestId('day-week-6')).toHaveTextContent('土')
+		test('test disable click event', () => {
+			const screen = render(<DatePainter selectedDates={[]} />)
+			const element = screen.getByTestId(`date-button-${day}`)
+			fireEvent.click(element)
+
+			const txtElement = screen.getByTestId(`date-txt-${day}`)
+			expect(txtElement).toHaveStyle('background: rba(0, 151, 230);')
+		})
+	})
+
+	describe('calendar holiday test', () => {
+		test('test style like holiday', () => {
+			const screen = render(<DatePainter selectedDates={[]} uniqueHoliday={['2021-09-23', '2021-08-09']} />)
+
+			const holidayTargetFirst = dayjs().year(2021).month(8).date(23)
+			const diffFirst = now.diff(holidayTargetFirst, 'month')
+
+			for (let i = 0; i < Math.abs(diffFirst); i++) {
+				fireEvent.click(screen.getByTestId('previous-button'))
+			}
+
+			expect(screen.getByTestId('date-txt-9/23')).toHaveClass('holiday')
+
+			const holidayTargetSecond = dayjs().year(2021).month(7).date(9)
+			const diffSecond = holidayTargetFirst.diff(holidayTargetSecond, 'month')
+
+			for (let i = 0; i < diffSecond; i++) {
+				fireEvent.click(screen.getByTestId('previous-button'))
+			}
+
+			expect(screen.getByTestId('date-txt-8/9')).toHaveClass('holiday')
+		})
 	})
 })
